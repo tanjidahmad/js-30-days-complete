fe564f44c093d3021a718f6f852d0b22ff41a202
tanjidahmad <tanjidahmad.cse.uiu@gmail.com>
2026-09-11 14:19:31 -0400

    operators

---
+++ operators.js
@@ -0,0 +1,59 @@
+//operator holo symbol ba keyword jeta deye js er value upore operation kora hoi 
+//first operator holo symbol
+// operand holo je value ba variable er upore operator kaj kore 
+
+let a=10
+let b=12
+console.log(a+b)
+// akahne let use korese ki type variavle sei jonno let holo block type jeta sudu block er modde update hoi arekta kaj ase seta holo value change kora jai ,, 
+// operend holo je value ba variable er upore operator kora hoi jemon aikhne a variable a and variable b er modde operator use hoise jekhane a holo operrand
+let price=500
+let quantity=2
+let total= price*quantity
+console.log(total)
+//operators 2 type hoi binary r unary
+// binary holo 2 ta operend lage r unary holo 1 ta operend lage
+// -x
+// x+y
+
+let x1=10
+let x2=20
+console.log(x1+x2)
+
+/// number er sathe boolean paile boolean ke number e convert kore but string thakle normal je vabe hour hoi
+/// nan number sata type er special value 
+// jekhane amra pure string like 'hello' dei tokon + sara jekono operator setake nan dhore 
+//not a number but print korle number asbe 
+
+console.log(2 ** 3);
+//arithmetic operator er last aita jeta power er kaj kore 
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 3);
+console.log(2 ** 4);
+console.log("10" + 5);
+console.log("10" - 5);
+// assingment operator holo kono variable er modde value rakha ba update kora ke bujai 
+let money=500
+money+=60
+console.log(money)
+
+//comparison operatores holo compare korbe 2 ta valu er modde 
+console.log(10 > 5);
+
+// >    greater than... 2 ta value soman hole true hoi na false hoi
+
+// <    less than... same as e greater er motoi 
+// >=   greater than or equal
+// <=   less than or equal
+// ==   loose equality    check only value
+
+// !=   loose inequality  
+
+// ===  strict equality   check value and datatype
+
+// !==  strict inequality
+
+///
